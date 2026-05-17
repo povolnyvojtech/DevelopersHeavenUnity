14a9db31d9a9db903597990ae396b5db273a9e03
povolny_vojtech <povolny.vojta007@gmail.com>
2026-05-17 18:27:44 +0200

    do job update
    TODO: when pressed correct keys it adds time although it shouldnt

--- Assets/Scripts/DoJob.cs
+++ Assets/Scripts/DoJob.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,55 +10,109 @@ using Random = UnityEngine.Random;
 public class DoJob : MonoBehaviour
 {
     public GameObject imagePrefab;
-    public Transform canvasTransform;
-    public Sprite[] sprites;
+    public Transform arrowsFieldTransform;
+    public Sprite[] greenArrows;
     public GameObject doJobCanvas;
     public TextMeshProUGUI counterDisplayerText;
     private List<(GameObject, KeyCode, KeyCode)> _imagesOnCanvas = new();
     private readonly List<string> _keyStringsList = new(){"A", "S", "D"};
+    private readonly KeyCode[] _letterKey = {KeyCode.A, KeyCode.S, KeyCode.D};
+    private readonly KeyCode[] _arrowKeys = {KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow};
     private KeyCode _currentDirectionKeyCode;
     private int _currentDirection; //0 - left, 1 - down, 2 - up, 3 - right
     private KeyCode _currentKeyKeyCode;
     private string _currentKey;
     private int _currentCount;
 
-    private void Start()
+    private void OnEnable()
     {
-        TimerManagerScript.JobFinished += () =>
-        {
-            doJobCanvas.SetActive(false);
-            GlobalVariables.CurrentJobTime = 0;
-            _currentCount = 0;
-        };
+        TimerManagerScript.JobFinished += EndJob;
         counterDisplayerText.text = _currentCount + "/" + GlobalVariables.NumberOfButtons;
         for (int i = 0; i < 2; i++)
         {
-            Color buttonColor = (i == 0) ? new Color(0, 1, 0) : new Color(1, 0, 0);
-            GameObject newImageObj = Instantiate(imagePrefab, canvasTransform);
+            Color buttonColor = i == 0 ? new Color(0,1,0) :  new Color(1,0,0);
+            GameObject newImageObj = Instantiate(imagePrefab, arrowsFieldTransform);
             Image imageComponent = newImageObj.GetComponent<Image>();
             TextMeshProUGUI keyText = newImageObj?.GetComponentInChildren<TextMeshProUGUI>();
         
             Vector3 imgPos = new Vector3(Random.Range(-810, 810), Random.Range(-390,390), 1);
-            _currentDirection = Random.Range(0, 4);
-            _currentKey = _keyStringsList[Random.Range(0, 3)];
+            int randomDirection = Random.Range(0, 4);
+            string randomKey = _keyStringsList[Random.Range(0, 3)];
+            while (randomDirection == _currentDirection)
+            {
+                randomDirection = Random.Range(0, 4);
+            }
+            while (randomKey == _currentKey)
+            {
+                randomKey = _keyStringsList[Random.Range(0, 3)];
+            }
+            _currentDirection = randomDirection;
+            _currentKey = randomKey;
             keyText.text = _currentKey;
             DetermineKeys(_currentDirection, _currentKey);
-            imageComponent.sprite = sprites[_currentDirection];
+            imageComponent.sprite = greenArrows[_currentDirection];
             imageComponent.color = buttonColor;
             newImageObj.transform.localPosition = imgPos;
             _imagesOnCanvas.Add((newImageObj, _currentDirectionKeyCode, _currentKeyKeyCode));
         }
     }
 
+    private void OnDisable()
+    {
+        TimerManagerScript.JobFinished -= EndJob;
+    }
+
+    private void EndJob()
+    {
+        doJobCanvas.SetActive(false);
+        GlobalVariables.CurrentJobTime = 0;
+        _currentCount = 0;
+    }
+
     private void Update()
     {
-        if (Input.GetKey(_imagesOnCanvas[0].Item2) && Input.GetKey(_imagesOnCanvas[0].Item3))
+        if (_imagesOnCanvas.Count == 0) return;
+
+        KeyCode correctKey1 = _imagesOnCanvas[0].Item2;
+        KeyCode correctKey2 = _imagesOnCanvas[0].Item3;
+
+        if (Input.GetKey(correctKey1) && Input.GetKey(correctKey2))
         {
             Destroy(_imagesOnCanvas[0].Item1);
             _imagesOnCanvas.RemoveAt(0);
+
             GenerateNextImage();
             RefreshButtonColors();
             counterDisplayerText.text = _currentCount + "/" + GlobalVariables.NumberOfButtons;
+            return;
+        }
+
+        KeyCode pressedArrow = KeyCode.None;
+        foreach (KeyCode arrow in _arrowKeys)
+        {
+            if (Input.GetKey(arrow))
+            {
+                pressedArrow = arrow;
+                break;
+            }
+        }
+
+        KeyCode pressedLetter = KeyCode.None;
+        foreach (KeyCode letter in _letterKey)
+        {
+            if (Input.GetKey(letter))
+            {
+                pressedLetter = letter;
+                break;
+            }
+        }
+
+        if (pressedArrow != KeyCode.None && pressedLetter != KeyCode.None)
+        {
+            if (pressedArrow != correctKey1 || pressedLetter != correctKey2)
+            {
+                DetermineTimePenalty();
+            }
         }
     }
 
@@ -68,33 +123,39 @@ public class DoJob : MonoBehaviour
             _imagesOnCanvas[i].Item1.GetComponent<Image>().color = (i == 0) ? new Color(0,1,0) : new Color(1,0,0);
         }
     }
+
+    private static void DetermineTimePenalty()
+    {
+        GlobalVariables.CurrentJobTimerSliderValue += GlobalVariables.NumberOfButtons switch
+        {
+            10 => 1,
+            15 => 2,
+            20 => 3,
+            25 => 4,
+            30 => 5,
+            35 => 6,
+            40 => 7,
+            45 => 8,
+            50 => 9,
+            _ => 0
+        };
+    }
     private static void DetermineNumberOfButtons()
     {
         Debug.Log(GlobalVariables.CurrentJobTime);
-        if (GlobalVariables.CurrentJobTime >= 10 && GlobalVariables.CurrentJobTime <= 20)
+        GlobalVariables.NumberOfButtons = GlobalVariables.CurrentJobTime switch
         {
-            GlobalVariables.NumberOfButtons = 10;
-        }
-        else if (GlobalVariables.CurrentJobTime >= 30 && GlobalVariables.CurrentJobTime <= 40)
-        {
-            GlobalVariables.NumberOfButtons = 15;
-        }
-        else if (GlobalVariables.CurrentJobTime >= 50 && GlobalVariables.CurrentJobTime <= 60)
-        {
-            GlobalVariables.NumberOfButtons = 20;
-        }
-        else if (GlobalVariables.CurrentJobTime >= 70 && GlobalVariables.CurrentJobTime <= 80)
-        {
-            GlobalVariables.NumberOfButtons = 25;
-        }
-        else if (GlobalVariables.CurrentJobTime >= 90 && GlobalVariables.CurrentJobTime <= 100)
-        {
-            GlobalVariables.NumberOfButtons = 30;
-        }
-        else
-        {
-            GlobalVariables.NumberOfButtons = 100;
-        }
+            >= 10 and <= 20 => 10,
+            > 20 and <= 30 => 15,
+            > 30 and <= 40 => 20,
+            > 40 and <= 50 => 25,
+            > 50 and <= 60 => 30,
+            > 60 and <= 70 => 35,
+            > 70 and <= 80 => 40,
+            > 80 and <= 90 => 45,
+            > 90 and <= 100 => 50,
+            _ => 100
+        };
     }
     
     public static void StartContract(int jobTime, int jobMoney, int jobXp)
@@ -110,24 +171,50 @@ public class DoJob : MonoBehaviour
 
     private void GenerateNextImage()
     {
-        if (_currentCount == GlobalVariables.NumberOfButtons)
+        if (_currentCount == GlobalVariables.NumberOfButtons-1)
         {
             TimerManagerScript.FinishJob(true);
             doJobCanvas.SetActive(false);
             GlobalVariables.CurrentJobTimerSliderValue = 0;
             _currentCount = 0;
+            foreach (var img in _imagesOnCanvas)
+            {
+                Destroy(img.Item1);
+            }
+            _imagesOnCanvas.Clear();
             return;
         }
-        GameObject newImageObj = Instantiate(imagePrefab, canvasTransform);
+        GameObject newImageObj = Instantiate(imagePrefab, arrowsFieldTransform);
         Image imageComponent = newImageObj.GetComponent<Image>();
         TextMeshProUGUI keyText = newImageObj.GetComponentInChildren<TextMeshProUGUI>();
-        
-        Vector3 imgPos = new Vector3(Random.Range(-810, 810), Random.Range(-390,390), 1);
-        _currentDirection = Random.Range(0, 4);
-        _currentKey = _keyStringsList[Random.Range(0, 3)];
+
+        Vector3 lastPos = _imagesOnCanvas[^1].Item1.GetComponent<RectTransform>().anchoredPosition;;
+        int offset = 250;
+        int x = Random.Range(-810, 810);
+        int y = Random.Range(-390, 390);
+        while (x >= (lastPos.x - offset) && x <= (lastPos.x + offset) && y >= (lastPos.y - offset) && y <= (lastPos.y + offset))
+        {
+            x = Random.Range(-810, 810);
+            y = Random.Range(-390, 390);
+        }
+        Debug.Log("Last pos: " +  lastPos + " new pos: " +  x + ", " + y);
+
+        var imgPos = new Vector3(x, y);
+        int randomDirection = Random.Range(0, 4);
+        string randomKey = _keyStringsList[Random.Range(0, 3)];
+        while (randomDirection == _currentDirection)
+        {
+            randomDirection = Random.Range(0, 4);
+        }
+        while (randomKey == _currentKey)
+        {
+            randomKey = _keyStringsList[Random.Range(0, 3)];
+        }
+        _currentDirection = randomDirection;
+        _currentKey = randomKey;
         keyText.text = _currentKey;
         DetermineKeys(_currentDirection, _currentKey);
-        imageComponent.sprite = sprites[_currentDirection];
+        imageComponent.sprite = greenArrows[_currentDirection];
         newImageObj.transform.localPosition = imgPos;
         _imagesOnCanvas.Add((newImageObj, _currentDirectionKeyCode, _currentKeyKeyCode));
         _currentCount++;
